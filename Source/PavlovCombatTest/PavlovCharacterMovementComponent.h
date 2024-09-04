64cab409ef3b06aac25173e4c4548ae904826624
Viktor Mendonck <viktor.mendonck@gmail.com>
2024-09-04 13:40:17 +0200

    remove networking
    no need for client side prediction or server authorative stuff in this game

--- Source/PavlovCombatTest/PavlovCharacterMovementComponent.h
+++ Source/PavlovCombatTest/PavlovCharacterMovementComponent.h
@@ -10,25 +10,6 @@ class PAVLOVCOMBATTEST_API UPavlovCharacterMovementComponent : public UCharacter
 {
 private:
 	GENERATED_BODY()
-#pragma region Networking shit
-	class FSavedMove_Pavlov : public FSavedMove_Character
-	{
-		uint8 bWantsToSprint:1;
-	
-		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
-		virtual void Clear() override;
-		virtual uint8 GetCompressedFlags() const override;
-		virtual void SetMoveFor(ACharacter* C, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
-		virtual void PrepMoveFor(ACharacter* C) override; 
-	};
-	class FNetworkPredictionData_Client_Pavlov : public FNetworkPredictionData_Client_Character
-	{
-		public:
-		FNetworkPredictionData_Client_Pavlov(const UCharacterMovementComponent& ClientMovement);
-		virtual FSavedMovePtr AllocateNewMove() override;
-	};
-#pragma endregion
-
 	
 	UPROPERTY(BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))bool bWantsToSprint;
 
@@ -36,14 +17,12 @@ private:
 	UPROPERTY(EditDefaultsOnly) float Walk_MaxWalkSpeed;
 public:
 	UPavlovCharacterMovementComponent();
-	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
 
 	UFUNCTION(BluePrintCallable) void SprintPressed();
 	UFUNCTION(BluePrintCallable) void SprintReleased();
 	
 	
 protected:
-	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
 
 	virtual void OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation, const FVector& OldVelocity) override;
 	
